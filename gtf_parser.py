c4a68edfa9e1841b3e0362faf5c0fc85249215ef
atch43 <49194835+atch43@users.noreply.github.com>
2019-10-12 13:00:57 +0200

    Add files via upload

--- gtf_parser.py
+++ gtf_parser.py
@@ -88,5 +88,58 @@ def _get_value(value):
 
     return value
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 df = dataframe("........")
 df[['feature','start','end']].head(100)
+
+
+
+new_df = df[['seqname','feature','start','end']].loc[df['feature'] == 'exon']
+
+new_df.start = pd.to_numeric(new_df.start, errors='coerce')
+new_df.end = pd.to_numeric(new_df.end, errors='coerce')
+# new_df = new_df.sort_values(by=['start'],ascending=True)
+new_df.head(5)
+
+
+lista={} #it's possible to create all the main keys (chr1, chr2...) before the operation and delete the control if(lista.get(a)==None)
+for filename in glob.iglob('D:\\bio\\kidney\\*.vcf'): #useful to open all vcf file in the script directory
+    allel.vcf_to_csv(f"{filename}", 'example.csv') #convert from vcf to csv 
+    with open('example.csv', mode='r') as f:
+        reader = csv.reader(f, delimiter=',')
+        included_cols = [0, 1, 2, 3, 4, 5, 6, 7, 8]      
+        for row in reader:
+            content = list(row[i] for i in included_cols)
+            
+            if row[8]=='True': 
+                a=row[0]              
+                if(lista.get(a) == None):
+                    lista[a]={}
+                    df_ch = new_df.loc[str(row[0]).split("chr")[1] == new_df['seqname']]
+                    df_start = df_ch.loc[new_df['start'] <= int(row[1])]
+                    df_end = df_start.loc[df_start['end'] >= int(row[1])]
+                    print(str(row[0])+ ": "+str(row[1])+': \r\n')
+                    print(df_end)
+                if(lista[a].get(row[1])==None):
+                    lista[a][row[1]]=1
+                else:
+                    lista[a][row[1]]+=1  #create the dictionary called "lista" with counter of mutations
+# json = json.dumps(lista) #convert "lista" in a json file to be saved
+# f = open("chr_map.json","w")
+# f.write(json)
+# f.close()
